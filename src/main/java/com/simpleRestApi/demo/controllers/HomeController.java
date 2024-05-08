899bec0c7797cad03bf33bb62b9c7f17da9928a4
KurumbaevaZh <144213447+KurumbaevaZh@users.noreply.github.com>
2024-05-08 08:29:26 +0300

    загружен проект

---
+++ src/main/java/com/simpleRestApi/demo/controllers/HomeController.java
@@ -0,0 +1,24 @@
+package com.simpleRestApi.demo.controllers;
+import com.simpleRestApi.demo.model.TemplateModel;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.http.ResponseEntity;
+import java.sql.Timestamp;
+import java.util.Date;
+@Controller
+@RequestMapping("/rest/home")
+public class HomeController {
+    @ResponseBody
+    @RequestMapping(value = "", method=RequestMethod.GET)
+    public ResponseEntity<TemplateModel> response(){
+        Date date = new Date();
+        Timestamp timestamp = new Timestamp(date.getTime());
+        TemplateModel model = new TemplateModel();
+        model.setMessage("json-ответ");
+        model.setTime(timestamp.toString());
+
+        return ResponseEntity.ok(model);
+    }
+}
